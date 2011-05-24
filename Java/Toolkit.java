c2be918209ca5f4f4a29d742081b1f2d68aefcde
Patrick de Kok <patrick@cxiu.nl>
2011-05-24 22:51:13 +0200

    Started working on interpreting the settings file.

---
+++ Java/Toolkit.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class Toolkit {
+  public static Properties getSettings(String... settingsPaths) {
+    Properties p = new Properties();
+    if (settingsPaths.length == 0) {
+      settingsPaths = new String[] {"../settings.ini"};
+    }
+
+    try {
+      for (String path : settingsPaths) {
+        p.load(new FileInputStream(path));
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Probably couldn't find the file.");
+    }
+
+    return p;
+  }
+}
