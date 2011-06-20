69dbdc775fe51b7a9f2bdac0c9e374a576c2125e
Patrick de Kok <patrick@cxiu.nl>
2011-06-20 16:19:15 +0200

    Beginning to setup the interaction with the GUI.

--- Java/src/nl/cxiu/thesis/JoystickActivity.java
+++ Java/src/nl/cxiu/thesis/JoystickActivity.java
@@ -2,6 +2,12 @@ package nl.cxiu.thesis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class JoystickActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,43 @@ public class JoystickActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final EditText input_ip = (EditText) findViewById(R.id.input_ip);
+        input_ip.setOnKeyListener(new OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  return true;
+                }
+                return false;
+            }
+        });
+        
+        final EditText input_port = (EditText) findViewById(R.id.input_port);
+        input_port.setOnKeyListener(new OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  return true;
+                }
+                return false;
+            }
+        });
+        
+        final ToggleButton tbSendData = (ToggleButton) findViewById(R.id.toggle_send_data);
+        tbSendData.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		if (tbSendData.isChecked()) {
+        			// start sending data
+        		}
+        		else {
+                    // stop sending data
+        		}
+        	}
+        });
     }
 }
