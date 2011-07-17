7557e6ce6cdf4ddfb0f395c5e7470905c2d807a0
Patrick de Kok <patrick@cxiu.nl>
2011-07-18 01:41:00 +0200

    Started on cleaning up input.py. Not finished yet :(

---
+++ threads.py
@@ -0,0 +1,104 @@
+import threading
+
+# TODO: What's up with exit()? It seems to wait until all non-main threads
+# have finished, and then starts calling atexit functions... 
+
+
+# TESTING!
+class WiimoteCallback(Callback):
+    def __init__(self, wiimote):
+        Callback.__init__(self, wiimote)
+
+    def register(self, callback_fn):
+        if hasattr(callback_fn, 'require'):
+            self.caller.rpt_mode ^= callback_fn.require
+        return Callback.register(self, callback_fn)
+
+
+class Callback(threading.Thread):
+    def __init__(self, caller):
+        threading.Thread.__init__(self)
+        self.caller = caller
+        self.callback_counter = 0
+        self.callback_functions = dict()
+        self.alive = True
+        self.waitlock = threading.Lock()
+        self.waitlock.acquire()
+
+    def terminate(self):
+        self.alive = False
+        if self.waitlock.locked():
+            self.waitlock.release()
+
+    def register(self, callback_fn):
+        if hasattr(callback_fn, '__iter__'):
+            return map(self.register, callback_fn)
+
+        if not self.callback_functions:
+            self.waitlock.release()
+        self.callback_counter += 1
+        self.callback_functions[self.callback_counter] = callback_fn
+        return self.callback_counter
+
+    def unregister(self, callback_id):
+        if callback_id in self.callback_functions:
+            del self.callback_functions[callback_id]
+        if not self.callback_functions and not self.waitlock.locked():
+            self.waitlock.acquire()
+
+    def notify(self, mesg):
+        for function in self.callback_functions.values():
+            if self.alive:
+                function(self.caller, mesg)
+            else:
+                return
+
+    def run(self):
+        previous_timestamp = 0
+        while self.alive:
+            self.waitlock.acquire()
+            self.waitlock.release()
+            mesg = self.caller.mesg
+            if mesg[0] > previous_timestamp:
+                self.notify(mesg)
+            previous_timestamp = mesg[0]
+
+
+# FIXME Seems broken. Don't know what goes wrong, actually. When connecting
+# through wiimote.Wiimote, the LEDs keep blinking.
+class PatternRumbler(threading.Thread):
+    def __init__(self, wiimote):
+        threading.Thread.__init__(self)
+        self.wiimote = wiimote
+        self.pattern = []
+        self.repeat = True
+        self.alive = True
+        self.keep_pattern = True
+
+    def terminate(self):
+        self.alive = False
+        self.keep_pattern = False
+
+    def stop(self):
+        return self.set_pattern([], True)
+
+    def set_pattern(self, pattern, repeat=False):
+        self.pattern = pattern
+        self.repeat = repeat
+        self.keep_pattern = False
+
+    def one_repeat(self):
+        for duration in self.pattern:
+            if not self.alive or not self.keep_pattern:
+                self.keep_pattern = True
+                return False
+            time.sleep(duration)
+            self.wiimote.rumble = not self.wiimote.rumble
+        return False
+
+    def run(self):
+        while self.alive:
+            if not self.one_repeat() or not self.repeat:
+                if self.keep_pattern:
+                    self.pattern = []
+                    self.wiimote.rumble = False
---
+++ wiimote.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+from threads import Callback, PatternRumbler
+
+import cwiid
+#import numpy
+
+import atexit
+#import collections
+#import math
+#import socket
+#import sys
+
+# TODO implement a "controller" model. It should implement at least:
+#    .is_calibrated()
+#    .calibrate([{"pos": (int, int), "size": int} OR None] * 4)
+#    .start_tracking()
+#    .stop_tracking()
+
+
+# I have two Wiimotes, and have marked one of them with a piece of tape. This
+# are their MAC addresses, for quick connections.
+WIIMOTE_NOTAPE = '00:1E:35:0F:4B:E9'
+WIIMOTE_TAPE = "00:25:A0:B3:00:EB"
+
+def t():
+    wiimote = Wiimote(WIIMOTE_NOTAPE)
+    wiimote.led = 5
+    return wiimote
+
+def require(rpt_mode):
+    def _require(callback_fn):
+        callback_fn.require = rpt_mode
+        return callback_fn
+    return _require
+
+@require(cwiid.RPT_BTN)
+def callback_disconnect(wiimote, timestamp, mesgs):
+    for mesg in mesgs:
+        if mesg[0] == cwiid.MESG_BTN\
+                and mesg[1] & cwiid.BTN_1 and mesg[1] & cwiid.BTN_2:
+            print "disconnecting"
+            wiimote.close()
+            return
+
+@require(cwiid.RPT_BTN)
+def callback_calibrate(controller):
+    def _callback_calibrate(wiimote, timestamp, mesgs):
+        if not controller.is_calibrated():
+            for mesg in mesgs: 
+                if mesg[0] == cwiid.MESG_BTN and mesg[1] & cwiid.BTN_HOME:
+                    wiimote.rpt_mode ^= cwiid.RPT_IR
+                    if controller.calibrate(wiimote.state['ir_src']):
+                        return
+    return _callback_calibrate
+
+@require(cwiid.RPT_BTN)
+def callback_trigger_tracking(controller):
+    def _callback_trigger_tracking(wiimote, timestamp, mesgs):
+        if controller.is_calibrated():
+            for mesg in mesgs:
+                if mesg[0] == cwiid.MESG_BTN and mesg[1] & cwiid.BTN_B:
+                    controller.start_tracking()
+                else:
+                    controller.stop_tracking()
+            pass
+    return _callback_trigger_tracking
+
+
+class DummyController(object):
+    calibration = False
+    @staticmethod
+    def is_calibrated(*args, **kwargs):
+        return Controller.calibration
+    @staticmethod
+    def calibrate(*args, **kwargs):
+        Controller.calibration = True
+    def start_tracking(self):
+        pass
+    def stop_tracking(self):
+        pass
+
+
+class Wiimote(object):
+    @property
+    def w(self):
+        return self.__wiimote
+
+    def __init__(self, bdaddr=None, led=0, rpt_mode=0):
+        if bdaddr is None:
+            self.__wiimote = cwiid.Wiimote()
+        else:
+            self.__wiimote = cwiid.Wiimote(bdaddr)
+        self.mesg = (0, [(None, None)])
+        def set_mesg(mesg, timestamp):
+            self.mesg = timestamp, mesg
+        self.__wiimote.mesg_callback = set_mesg
+        self.__wiimote.enable(cwiid.FLAG_MESG_IFC)
+
+        self.led = led
+        self.rpt_mode = rpt_mode
+
+        self._pattern_rumble = PatternRumbler(self)
+        # FIXME See threads.PatternRumbler
+        #self._pattern_rumble.start()
+        self._callback = Callback(self)
+        self._callback.start()
+
+        atexit.register(self.close)
+
+    def close(self):
+        self._callback.terminate()
+        self._pattern_rumble.terminate()
+        self.__wiimote.close()
+
+    def register_callback(self, fn):
+        self._callback.register(fn)
+
+    def unregister_callback(self, fn_id):
+        self._callback.unregister(fn_id)
+
+    def notify_callbacks(self):
+        self._callback.notify(mesg)
+
+    def rumble_pattern(self, pattern, repeat=False):
+        return self._pattern_rumble.set_pattern(pattern, repeat)
+
+    def rumble_stop(self):
+        return self._pattern_rumble.stop()
+
+    @property
+    def rumble(self):
+        return self._rumble
+    
+    @rumble.setter
+    def rumble(self, value):
+        self._rumble = value
+        self.__wiimote.rumble = self._rumble
+
+    @property
+    def state(self):
+        return self.__wiimote.state
+
+    @property
+    def led(self):
+        return self._led
+
+    @led.setter
+    def led(self, value):
+        self._led = value
+        self.__wiimote.led = self._led
+
+    @property
+    def rpt_mode(self):
+        return self._rpt_mode
+
+    @rpt_mode.setter
+    def rpt_mode(self, value):
+        self._rpt_mode = value
+        self.__wiimote.rpt_mode = self._rpt_mode
